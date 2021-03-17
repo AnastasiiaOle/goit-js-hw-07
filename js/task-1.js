61dcb3bace1685e2026b081be006e4dc858cf095
Anastasiia Oleinikova <anastasiiaole@gmail.com>
2021-03-17 22:09:10 +0100

    updated

--- js/task-1.js
+++ js/task-1.js
@@ -8,3 +8,15 @@ const categoriesArray = [...totalCategories]
   )
   .join("\n");
 console.log(categoriesArray);
+
+// const mainListRef = document.querySelector('#categories');
+// const mainListItems = Array.from(mainListRef.children);
+
+// console.log(`В списке ${mainListItems.length} категории.`);
+
+// mainListItems.forEach(el => {
+//   console.log(`Категория: ${el.querySelector('h2').textContent}`);
+//   console.log(
+//     `Количество элементов: ${el.querySelector('ul').children.length}`,
+//   );
+// });
--- js/task-2.js
+++ js/task-2.js
@@ -19,3 +19,22 @@ const liRefs = ingredients.map(item => {
 
 ingRef.append(...liRefs);
 
+  
+// const ingredients = [
+//   'Картошка',
+//   'Грибы',
+//   'Чеснок',
+//   'Помидоры',
+//   'Зелень',
+//   'Приправы',
+// ];
+
+// const createListItems = listItems =>
+//   listItems.map(function (item) {
+//     const itemRef = document.createElement('li');
+//     itemRef.textContent = item;
+//     return itemRef;
+//   });
+
+// const listRef = document.querySelector('#ingredients');
+// listRef.append(...createListItems(ingredients));
